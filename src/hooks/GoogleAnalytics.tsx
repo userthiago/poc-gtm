56eb7e0af2a8e6e24fed0f7d0e50eec5189b364a
Thiago <thiagosantosprofissional@gmail.com>
2021-07-05 11:43:11 -0300

    feat: Adicionando hook que emite mensagens ao Google Analytics.

---
+++ src/hooks/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { GA_TRACKING_ID } from '../../helpers/consts';
+
+declare global {
+  interface Window {
+    GA_INITIALIZED: boolean;
+  }
+}
+
+export function usePageViews(): void {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      ReactGA.initialize(GA_TRACKING_ID);
+      window.GA_INITIALIZED = true;
+    }
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }, [location]);
+}
